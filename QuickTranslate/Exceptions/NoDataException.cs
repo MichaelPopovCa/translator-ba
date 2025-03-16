36793c90b64365707c27e267c043b59f118348e4
Michael6138 <natanpopov6138@gmail.com>
2025-03-16 09:34:07 -0600

    fdfdflll

---
+++ QuickTranslate/Exceptions/NoDataException.cs
@@ -0,0 +1,15 @@
+﻿using QuickTranslate.Enums;
+
+namespace QuickTranslate.Exceptions
+{
+    public class NoDataException : Exception
+    {
+        public TranslationErrorCode TranslationErrorCode { get; }
+
+        public NoDataException(string message, TranslationErrorCode translationErrorCode)
+        : base(message)
+        {
+            TranslationErrorCode = translationErrorCode;
+        }
+    }
+}
---
+++ QuickTranslate/Models/Request/TranslationRequest.cs
@@ -0,0 +1,18 @@
+﻿namespace QuickTranslate.Models.Request
+{
+    public class TranslationRequest : ITranslationRequest
+    {
+        public int TranslatorType { get; }
+        public string SourceLanguage { get; }
+        public string SourceText { get; }
+        public string TargetLanguage { get; }
+
+        public TranslationRequest(int translatorType, string sourceLanguage, string sourceText, string targetLanguage)
+        {
+            TranslatorType = translatorType;
+            SourceLanguage = sourceLanguage;
+            SourceText = sourceText;
+            TargetLanguage = targetLanguage;
+        }
+    }
+}
